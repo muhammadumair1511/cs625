3bc4de9e0de65203ed8abc04d2fda745d9adfdf1
muhammadumair1511 <48052126+muhammadumair1511@users.noreply.github.com>
2019-03-15 23:40:35 +0500

    git commits

---
+++ GaltoLit.java
@@ -0,0 +1,18 @@
+public class GaltoLit{
+	public static void main(String[] args){
+		
+
+
+		double gallons;		
+		double liters;		
+
+		liters = 10000;	
+
+		gallons = liters / 3.7854; 
+
+		System.out.println(liters + " liters in " + gallons + " gallons.");
+
+
+
+	}
+}
